19c2377f3616d1e0c0cdd02ae9eef03657dfb24d
Beam <1105897748@qq.com>
2019-05-14 16:26:07 +0800

    basic page and collect function

---
+++ js/website-data.js
@@ -0,0 +1,18 @@
+var website_url = 
+{
+	"图书馆" : "http://lib.scnu.edu.cn",
+	"计算机学院" : "http://cs.scnu.edu.cn",
+	"教务管理系统" : "http://jwc.scnu.edu.cn",
+	"纪委监察处" : "http://jwjc.scnu.edu.cn",
+	"组织部" : "http://hsdj.scnu.edu.cn"
+};
+
+
+var website_english =
+{
+	"图书馆" : "lib",
+	"计算机学院" : "cs",
+	"教务管理系统" : "jwc",
+	"纪委监察处" : "jwjc",
+	"组织部" : "hsdj"
+}
