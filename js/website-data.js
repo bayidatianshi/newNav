39879d3663b42d24bed6039effb58e8cbd65c96f
C-xym <18924772423@163.com>
2019-06-02 00:14:04 +0800

    190602

--- js/website-data.js
+++ js/website-data.js
@@ -1,18 +1,254 @@
 var website_url = 
 {
-	"图书馆" : "http://lib.scnu.edu.cn",
-	"计算机学院" : "http://cs.scnu.edu.cn",
-	"教务管理系统" : "http://jwc.scnu.edu.cn",
+	"党委办公室、校长办公室" : "http://office.scnu.edu.cn",
+	"组织部" : "http://hsdj.scnu.edu.cn",
 	"纪委监察处" : "http://jwjc.scnu.edu.cn",
-	"组织部" : "http://hsdj.scnu.edu.cn"
+	"人事处" : "http://rsc.scnu.edu.cn",
+	"学生工作部（处）、人民武装部" : "http://xsb.scnu.edu.cn",
+	"教务处" : "http://jw.scnu.edu.cn",
+	"国际交流合作处" : "http://io.scnu.edu.cn",
+	"基建处" : "http://jjc.scnu.edu.cn",
+	"保卫处" : "http://bwc.scnu.edu.cn",
+	"科技处" : "http://kjc.scnu.edu.cn/business/website/mainPageIndex.do",
+	"招生考试处" : "http://zkc.scnu.edu.cn",
+	"省高校师资培训中心" : "http://gdsz.scnu.edu.cn",
+	"研究生院（处）" : "http://yjsy.scnu.edu.cn",
+	"社科处" : "http://skc.scnu.edu.cn",
+	"财务处" : "http://hscw.scnu.edu.cn",
+	"资产管理处" : "http://amd.scnu.edu.cn",
+	"后勤管理处" : "http://szhq.scnu.edu.cn/dsh",
+	"校团委" : "http://zijingwang.org/xtw",
+	"校工会" : "http://gh.scnu.edu.cn/aprescms",
+	"发展规划处" : "http://fzghb.scnu.edu.cn",
+	"社会服务处" : "http://shfw.scnu.edu.cn",
+	"离退休工作办公室" : "http://ltb.scnu.edu.cn",
+	"审计处" : "http://portal.scnu.edu.cn/wps/portal/sjc",
+	"机关党委" : "http://jgdw.scnu.edu.cn",
+
+	"文学院" : "http://wxy.scnu.edu.cn",
+	"历史文化学院" : "http://lswh.scnu.edu.cn",
+	"法学院" : "http://fxy.scnu.edu.cn",
+	"政治与行政学院" : "http://zhx.scnu.edu.cn",
+	"马克思主义学院" : "http://mks.scnu.edu.cn",
+	"经济与管理学院" : "http://em.scnu.edu.cn",
+	"教育科学学院" : "http://jky.scnu.edu.cn",
+	"心理学院" : "http://psy.scnu.edu.cn",
+	"外国语言文化学院" : "http://sfs.scnu.edu.cn",
+	"公共管理学院" : "http://ggy.scnu.edu.cn",
+	"体育科学学院" : "http://sport.scnu.edu.cn",
+	"生命科学学院" : "http://life.scnu.edu.cn",
+	"教育信息技术学院" : "http://site.scnu.edu.cn",
+	"信息光电子科技学院" : "http://ioe.scnu.edu.cn",
+	"网络教育学院" : "http://www.gdou.com",
+	"继续教育学院" : "http://cce.scnu.edu.cn",
+	"物理与电信工程学院" : "http://physics.scnu.edu.cn",
+	"数学科学学院" : "http://maths.scnu.edu.cn",
+	"化学与环境学院" : "http://sce.scnu.edu.cn",
+	"计算机学院" : "http://cs.scnu.edu.cn",
+	"地理科学学院" : "http://geography.scnu.edu.cn",
+	"音乐学院" : "http://music.scnu.edu.cn",
+	"美术学院" : "http://aol.scnu.edu.cn",
+	"基础教育培训与研究院" : "http://jjy.scnu.edu.cn",
+	"旅游管理学院" : "http://stm.scnu.edu.cn",
+	"国际文化学院" : "http://cicgz.scnu.edu.cn",
+	"国际商学院" : "http://ibc.scnu.edu.cn",
+	"软件学院" : "http://ss.scnu.edu.cn",
+	"城市文化学院" : "http://cswh.scnu.edu.cn",
+	"凤凰国际学院" : "http://phoenix.scnu.edu.cn",
+	"职业教育学院" : "http://zyjy.scnu.edu.cn",
+	"创业学院" : "http://es.scnu.edu.cn",
+	"脑科学与康复医学研究院" : "http://ibrr.scnu.edu.cn",
+
+
+	//科研机构
+	"教科所" : "http://esi.scnu.edu.cn",
+	"激光生命科学实验室" : "http://laser.scnu.edu.cn",
+	"经济行为科学重点实验室" : "http://sleb.scnu.edu.cn",
+	"植物发育生物工程实验室" : "http://sky.scnu.edu.cn/zdsys/zwfy/keylab2004/index0.htm",
+
+	"光电子材料与技术研究所" : "http://iomt.scnu.edu.cn",
+	"光子信息技术实验室" : "http://202.116.32.249/gxjs",
+	"华南市场经济研究中心" : "http://hnjj.scnu.edu.cn",
+	"华南先进光电子研究院" : "http://aoe.scnu.edu.cn",
+
+	"生物光子学研究院" : "http://biop.scnu.edu.cn",
+	"城市与区域发展研究中心" : "http://curds.scnu.edu.cn",
+	"运动科学实验教学示范中心" : "http://sportlab.scnu.edu.cn",
+	"系统科学与系统管理研究中心" : "http://sssmcenter.scnu.edu.cn",
+
+	"光及电磁波研究中心" : "http://coer.scnu.edu.cn",
+	"文化产业与文化地理研究中心" : "http://cscg.scnu.edu.cn",
+
+
+	//辅教机构
+	"网络中心" : "http://nc.scnu.edu.cn",
+	"图书馆" : "http://lib.scnu.edu.cn",
+	"档案馆" : "http://dag.scnu.edu.cn",
+	"分析测试中心" : "http://atc.scnu.edu.cn",
+
+	"教师发展中心" : "http://ctld.scnu.edu.cn",
+
+	//学术机构
+	"华南师大学报" : "http://journal.scnu.edu.cn",
+	"体育学刊" : "http://www.chinatyxk.com",
+
+	//校园应用
+	"华师百科" : "http://wiki.scnu.edu.cn",
+	"华师新陶园论坛" : "http://bbs.scnu.edu.cn",
+	"华师陶园PT" : "http://pt.scnu.edu.cn",
+	"邮箱服务" : "http://mail.scnu.edu.cn",
+
+	"综合服务平台" : "http://sso.scnu.edu.cn",
+	"移动平台" : "http://app.scnu.edu.cn",
+	"校园网自助服务平台" : "http://sso.scnu.edu.cn/selfservice",
+	"校园视频网" : "http://222.200.129.36",
+
+	"锐捷客户端下载" : "http://statics.scnu.edu.cn/ruijie/index.htm",
+
+	//学生服务
+	"教务管理系统" : "http://jwc.scnu.edu.cn",
+	"教学信息查询系统" : "http://222.201.93.5:211",
+	"学生工作信息管理系统" : "http://ssp.scnu.edu.cn",
+	"课程中心" : "http://202.116.47.190/course_center/index",
+
+	"教务处" : "http://jw.scnu.edu.cn",
+	"图书馆" : "http://lib.scnu.edu.cn",
+	"勤工助学管理中心" : "http://www.scnuqg.com",
+	"学生就业指导中心" : "http://career.scnu.edu.cn",
+
+	"心理咨询研究中心" : "http://www.psyonline.cn",
+	"学生会" : "http://stuu.scnu.edu.cn",
+
+	//其他
+	"教育发展中心" : "http://jyfzzx.scnu.edu.cn",
+	"电子化政府采购系统" : "http://www.gdgpo.gov.cn",
+	"华南师大附属幼儿园" : "http://yey.scnu.edu.cn",
+	"校医院" : "http://hospital.scnu.edu.cn",
+
+	"教育审计协会" : "http://geaa.scnu.edu.cn"
 };
 
 
 var website_english =
 {
-	"图书馆" : "lib",
-	"计算机学院" : "cs",
-	"教务管理系统" : "jwc",
-	"纪委监察处" : "jwjc",
-	"组织部" : "hsdj"
+	"党委办公室、校长办公室" : "President's Office of SCNU",
+	"组织部" : "Organization Department,SCNU",
+	"纪委监察处" : "Commission for Discipline Inspection,SCNU",
+	"人事处" : "Human Resources Department,SCNU",
+	"学生工作部（处）、人民武装部" : "Student Affairs Department, people's armed forces department,SCNU",
+	"教务处" : "Dean's Office,SCNU",
+	"国际交流合作处" : "Office Of International Exchange and Cooperation,SCNU",
+	"基建处" : "Capital Construction Department,SCNU",
+	"保卫处" : "BWC,SCNU",
+	"科技处" : "Technology Department,SCNU",
+	"招生考试处" : "Entrance Examination Committee Office",
+	"省高校师资培训中心" : "Guangdong Teachers Training Center For Institutions of Higher Learning",
+	"研究生院（处）" : "Graduate School,SCNU",
+	"社科处" : "Office of Humanities and Social Sciences Division,SCNU",
+	"财务处" : "Finance Department,SCNU",
+	"资产管理处" : "Asset Management Office,SCNU",
+	"后勤管理处" : "Logistics Management Office,SCNU",
+	"校团委" : "Youth League,SCNU",
+	"校工会" : "Trade Union,SCNU",
+	"发展规划处" : "Development and Planning Department,SCNU",
+	"社会服务处" : "Social Service,SCNU",
+	"离退休工作办公室" : "Retired Work Office,SCNU",
+	"审计处" : "Audit Office,SCNU",
+	"机关党委" : "Party Committee,SCNU",
+
+	"文学院" : "School of Literature",
+	"历史文化学院" : "School of History and Culture",
+	"法学院" : "School of Law",
+	"政治与行政学院" : "School of Politics and Administration",
+	"马克思主义学院" : "School of Marxism",
+	"经济与管理学院" : "School of Economics and Management",
+	"教育科学学院" : "School of Education",
+	"心理学院" : "School of Psychology",
+	"外国语言文化学院" : "School of Foreign Studies",
+	"公共管理学院" : "School of Public Administration",
+	"体育科学学院" : "School of Physical Education and Sports Science",
+	"生命科学学院" : "School of Life Sciences",
+	"教育信息技术学院" : "School of Information Technology in Education",
+	"信息光电子科技学院" : "School of Information and Optoelectronic Science and Engineering",
+	"网络教育学院" : "School of Network Education",
+	"继续教育学院" : "College of Continuing Education",
+	"物理与电信工程学院" : "School of Physics and Telecommunications Engineering",
+	"数学科学学院" : "School of Mathematical Sciences",
+	"化学与环境学院" : "School of Chemistry and Environment",
+	"计算机学院" : "School of Computer Science",
+	"地理科学学院" : "School of Geography",
+	"音乐学院" : "School of Music",
+	"美术学院" : "School of Fine Arts",
+	"基础教育培训与研究院" : "School of Professional Development and Research on Primary and Secondary Education",
+	"旅游管理学院" : "School of Tourism Management",
+	"国际文化学院" : "College of International Culture",
+	"国际商学院" : "International Business College",
+	"软件学院" : "School of Software",
+	"城市文化学院" : "School of Urban Culture",
+	"凤凰国际学院" : "Phoenix International College",
+	"职业教育学院" : "Vocational Education College",
+	"创业学院" : "School of Entrepreneurship",
+	"脑科学与康复医学研究院" : "Institute for Brain Research and Rehabilitation",
+
+
+	//科研机构
+	"教科所" : "Institute of Education",
+	"激光生命科学实验室" : "Key Laboratory of Laser Life Science, Ministry of Education",
+	"经济行为科学重点实验室" : "Key Laboratory of Economic Behavioral Science",
+	"植物发育生物工程实验室" : "Plant Development Bioengineering Laboratory",
+	"光电子材料与技术研究所" : "Institute of Optoelectronic Materials and Technology",
+	"光子信息技术实验室" : "Photonic Information Technology Laboratory",
+
+
+
+	"华南市场经济研究中心" : "South China Market Economy Research Center",
+	"华南先进光电子研究院" : "South China Advanced Photonics Research Institute",
+	"生物光子学研究院" : "Biophotonics Institute",
+	"城市与区域发展研究中心" : "Urban and Regional Development Research Center",
+	"运动科学实验教学示范中心" : "Sports Science Experimental Teaching Demonstration Center",
+	"系统科学与系统管理研究中心" : "Systems Science and Systems Management Research Center",
+	"光及电磁波研究中心" : "Optical and Electromagnetic Wave Research Center",
+	"文化产业与文化地理研究中心" : "Cultural Industry and Cultural Geography Research Center",
+
+
+	//辅教机构
+	"网络中心" : "Network Center",
+	"图书馆" : "library",
+	"档案馆" : "Archives",
+	"分析测试中心" : "Analytical test center",
+	"教师发展中心" : "Teacher development center",
+
+	//学术机构
+	"华南师大学报" : "Journal of South China Normal University",
+	"体育学刊" : "Sports journal",
+
+	//校园应用
+	"华师百科" : "WIKI.SCNU",
+	"华师新陶园论坛" : "Huashi New Ceramic Garden Forum",
+	"华师陶园PT" : "Huashi Taoyuan PT",
+	"邮箱服务" : "Mailbox service",
+	"综合服务平台" : "Integrated service platform",
+	"移动平台" : "mobile platform",
+	"校园网自助服务平台" : "Campus network self-service platform",
+	"校园视频网" : "Campus video network",
+	"锐捷客户端下载" : "Ruijie client download",
+
+	//学生服务
+	"教务管理系统" : "Educational management system",
+	"教学信息查询系统" : "Teaching information inquiry system",
+	"学生工作信息管理系统" : "Student work information management system",
+	"课程中心" : "Course center",
+	"教务处" : "Academic Affairs Office",
+	"图书馆" : "library",
+	"勤工助学管理中心" : "Work-study management center",
+	"学生就业指导中心" : "Student Employment Guidance Center",
+	"心理咨询研究中心" : "Counseling Research Center",
+	"学生会" : "Student union",
+
+	//其他
+	"教育发展中心" : "Education Development Center",
+	"电子化政府采购系统" : "Electronic government procurement system",
+	"华南师大附属幼儿园" : "South China Normal University Affiliated Kindergarten",
+	"校医院" : "school clinic",
+	"教育审计协会" : "Educational Audit Association"
 }
